f9838ca7bd45c034863e70ec18b21a50f0c51fb0
Enrico Ferrero <enricoferrero86@gmail.com>
2013-12-20 14:29:18 +0000

    solved MPRT

--- MPRT.pl
+++ MPRT.pl
@@ -2,39 +2,32 @@
 use Modern::Perl;
 use Bio::DB::SwissProt;
 
-my @IDs;
-my @newIDs;
+my @rosalindIds;
+my @swissProtIds;
 while (<>) {
 	chomp;
-	push @IDs, $_;
+	push @rosalindIds, $_;
 	s/(.+?)_.+/$1/ if /_/;
-	push @newIDs, $_;
+	push @swissProtIds, $_;
 }
 
-my $SPobj = Bio::DB::SwissProt->new;
-my $seqStream = $SPobj->get_Stream_by_id(\@newIDs);
+my $swissProtObj = Bio::DB::SwissProt->new;
 
 my %matches;
-while (my $seqObj = $seqStream->next_seq) {
+for my $i (0 .. $#swissProtIds) {
+	my $seqObj= $swissProtObj->get_Seq_by_acc($swissProtIds[$i]);
 	my $seq = $seqObj->seq;  # I have no idea why this is required
-	while ($seq =~ /N[^P][ST][^P]/g) {
-		my $SPid = $seqObj->accession_number;
+	while ($seq =~ /(?=N[^P][ST][^P])/g) {
+		my $id = $rosalindIds[$i];
 		my $pos = $-[0] + 1;
-		push @{ $matches{$SPid} }, $pos;
+		push @{ $matches{$id} }, $pos;
 	}
 }
 
-for my $SPid (keys %matches) {
-	for my $ID (@IDs) {
-		if ($ID =~ /$SPid/) {
-			say $ID;
-		}
-		elsif ($SPid =~ /$ID/) {
-			say $ID;
-		}
-	}
-	for my $i (0 .. $#{ $matches{$SPid} }) {
-		print $matches{$SPid}[$i], " ";
+for my $id (keys %matches) {
+	say $id;
+	for my $i (0 .. $#{ $matches{$id} }) {
+		print $matches{$id}[$i], " ";
 	}
 	print "\n";
 }
